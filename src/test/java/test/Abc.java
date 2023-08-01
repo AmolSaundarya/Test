a7c58e3c53e40b92b16686fa961626a2d5cfe735
AmolSaundarya <123649795+AmolSaundarya@users.noreply.github.com>
2023-08-01 12:59:22 +0530

    [Amol] Review New Tc

--- src/test/java/test/Abc.java
+++ src/test/java/test/Abc.java
@@ -1,10 +1,15 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Abc {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		 WebDriver driver=new ChromeDriver();
+		  driver.get("https://www.amazon.in/");
+		  Thread.sleep(3000);
+		  driver.quit();
 	}
 
 }
